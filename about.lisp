5f4c1b4992dbba43f8a33d7e8c2a4f2dc98c5dca
digikar99 <shubhamkarayare@gmail.com>
2019-12-19 13:47:05 +0530

    testing pre-commit hook

--- about.lisp
+++ about.lisp
@@ -2,10 +2,6 @@
 
 (reader:enable-reader-syntax 'lambda)
 
-;; (load "my-markup.lisp")
-
-(in-package :cl-user)
-
 (setf (macro-function '$) (macro-function 'cl-markup:markup))
 
 (defmacro with-tidy-xml (file-name string)
@@ -23,19 +19,21 @@
   ($ `(:link :rel "stylesheet" :href ,filename)))
 
 (defparameter *about-text*
-  (cl-markup:html5
-    (:head
-     (:meta :charset "utf-8")
-     (:meta :name "viewport" :content "width=device-width, initial-scale=1, shrink-to-fit=no")
-     (include-css "index.css")
-     (:title "Shubhamkar B. Ayare (digikar)"))
-    (:body
-     (:div :id "defining-pic" (:img :src "defining-pic"))
-     (:div :id "profile-pic" (:img :src "profile-pic"))
-     (:div :id "note"
-           "[This page introduces me in a formal setting. "
-           "For an informal introduction, please visit "
-           (:a :href "" "this site ")
-           ".]"))))
+  (let ((cl-markup:*auto-escape* nil))
+    (cl-markup:html5
+      (:head
+       (:meta :charset "utf-8")
+       (:meta :name "viewport"
+              :content "width=device-width, initial-scale=1, shrink-to-fit=no")
+       (include-css "index.css")
+       (:title "Shubhamkar B. Ayare (digikar)"))
+      (:body
+       (:div :id "defining-pic" (:img :src "defining-pic"))
+       (:div :id "profile-pic" (:img :src "profile-pic"))
+       (:div :id "note"
+             "[This page is intended to introduce me in a formal setting. "
+             "For an informal introduction, please visit "
+             (:a :href "" "this site ")
+             ".]")))))
 
 (with-tidy-xml "index.html" *about-text*)
