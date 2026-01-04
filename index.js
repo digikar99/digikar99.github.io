60315c4ff1b87b4f952c5843c4a3b8158239974b
Shubhamkar Ayare <digikar@proton.me>
2026-01-04 07:46:34 +0100

    [fix] js: use window.slides instead of slides

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ var slides;
 var quotes_request = new Request("random-quotes.txt");
 fetch(quotes_request).then(function(response) {
     return response.text().then(function(text) {
-        slides = text.split("\n");
+        window.slides = text.split("\n");
     });
 });
 
 function advanceSlides(){
     var slidesContainer = document.getElementsByClassName("slideshow-container")[0];
-    var numSlides = slides.length;
+    var numSlides = window.slides.length;
     var slideIndex = Math.floor(Math.random() * numSlides);
     if (slideIndex >= numSlides) slideIndex = 0;
-    slidesContainer.innerHTML = "<quote>\"" + slides[slideIndex] + "\"</quote>";
-    if (slides[slideIndex] == "") advanceSlides();
+    slidesContainer.innerHTML = "<quote>\"" + window.slides[slideIndex] + "\"</quote>";
+    if (window.slides[slideIndex] == "") advanceSlides();
 }
 
 window.onload = function (e) {
